92bc0751f4cd7f120e97e8e69fc8984937fbdd16
Ashutosh Kumar <ashutoshk043@gmail.com>
2025-07-24 15:01:22 +0530

    implemented ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { SurveysComponent } from './pages/surveys/surveys.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { WebStoriesComponent } from './pages/web-stories/web-stories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', component: HomeComponent },
+  { path: 'web-stories', component: WebStoriesComponent },
+  { path: 'surveys', component: SurveysComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'terms', component: TermsComponent },
+];
---
+++ src/app/pages/web-stories/web-stories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-stories',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-stories.component.html',
+  styleUrl: './web-stories.component.scss'
+})
+export class WebStoriesComponent {
+
+}
